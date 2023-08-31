29e73fa56f0189e484351c4805b08c0599d4acff
Vinicius Campos <viniciuscampos@pop-os.localdomain>
2023-08-30 22:07:06 -0300

    Correcao

--- GentCult/settings.py
+++ GentCult/settings.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -14,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
